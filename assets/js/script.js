c6bc3927e1e83b579725ac0636e1b9700af8ad04
paolo <paoloquintiliani91@gmail.com>
2024-07-22 10:25:55 -0400

    ejemplo de javascript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+console.log("Hola, empezando con JS");
+
+/* declaracion */
+var cadena1;
+
+/* inicializar */
+cadena1="dato de ejemplo";
+
+/* invocar */
+console.log(cadena1);
+
+/* reasignar */
+cadena1="Nuevo dato";
+
+/* declarar e inicializar */
+var cadena2 = "Declarada e inicializada en un solo paso";
+
+
+/* funciones preescritas 
+alert();
+prompt();
+parseFloat();
+parseInt();
+document.getElementById(); */
