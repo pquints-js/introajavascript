8b12681036446ba597b08a10508d40f3f7421ae1
paolo <paoloquintiliani91@gmail.com>
2024-07-22 12:14:17 -0400

    ejemplos de javascript ok

--- README.md
+++ README.md
@@ -2,3 +2,37 @@
 ## Ejemplo visto en clases
 
 Hola, haciendo introduccion a javascript
+<Javascript/>
+
+unico lenguaje de programacion para front-end
+estandar - front: "el lenguaje para esto, hace proceso html es para agregar"
+debilmente tipado: ------------> tipo de datos (variable)permite "hacer lo que quieras"
+orientado a objetos
+lenguaje interpretado: traductor para ejecutarse js-java-la mayoria:
+asincrono (mas de 1 tarea a la vez): hilos de ejecucion
+multiplataforma: front-back
+se encarga de interaccion o dinamismo por el lado del cliente
+funciona en base a algoritmos
+ECMA script: actualizaciones de javascript (6 ultimo actualizacion la "mas importante")
+compatibilidad: "que se interpretado le da una alta compatibilidad. ejecutar js en navegadores desde dispositivos(pc,consola, movil,etc)"
+
+funcion preescrita: que es? console.log
+typescript----javascript crtl+click en log o en funciones del script 
+
+variable: permiten recibir un dato de entrada, procesarlo y sacarlo
+no son persistente, "son volatiles", almacenados en memoria ram
+para guardarlo debe ser en base de datos
+
+*var* como son declaradas recepcionar un dato
+3 pasos
+
+declararla: "asignar un id alias etiqueta", "reserva espacio memoria para usar"
+declarar: var cadena;
+inicializar: cadena = "dato"; se le asigna un dato a la cadena
+invocar: console.log(cadena); cadena la voy a mostrar a la consola del navegador
+reasignar - setear: cadena="nuevo dato";
+
+tipo de dato
+booleano
+true false
+etc
--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,59 @@ var montoIngresado = 10000;  /* si lo ponemos en comillas concatenan y estariamo
 var saldo = 10000;
 saldo = saldo + montoIngresado; // 20000
 
+/* define tipo de dato */
+var cadenaCaracteres = "definiendo una variable como String";
+var numeroEntero = 456;
+var numeroDecimal = 5.4;
+var booleanUno = true;
+var booleanDos = false;
+var booleanOperacion = 5 == 4;
 
+
+/* Operarla */
+/* Operadores */
+/* Aritmeticos */
+/* + - / x */
+/* suma, restas, multiplicacion, division, modulo(resto) */
+/* Modulo */
+/* 2 / 2 = 1 
+    |0*/
+/* 3 / 2 = 1 
+    |1*/
+
+/* Logicos operando 2 valores daran da igual como sea sera 1 booleano*/
+/*  == != === !== <> <= >= */
+console.log(4 == 5); //false
+console.log(4 != 5); //true
+console.log(4 < 5); //true
+console.log(4 >= 5); //false
+console.log(4 <= 5); //true
+console.log(5 > 5); //false
+console.log("2" == 2); //true
+console.log("2" === 2); //false
+
+/* Cadena de caracteres */
+/* +; concatenar */
+var datoIncrustar = 'ESTE ES EL DATO QUE PUEDE CAMBIAR'
+var cadenaDinamica = 'Cadena con un dato que cambia segun valor de la variable' + datoIncrustar
+console.log(cadenaDinamica);
+
+Interpolacion 
+var cadenaInterpolacion = `Cadena con un dato que cambia segun de la variable ${datoIncrustar}`;
+console.log(cadenaInterpolacion);
+
+/* Ejemplo de diferencia entre concatenar e interpolar */
+var numeroUno = 5;
+var numeroDos = 7;
+var suma = numeroUno + numeroDos;
+
+/* Concatenacion */
+var mensaje = 'El resultado de la suma de' + numeroUno + '+' + numeroDos + '=' + suma;
+console.log(mensaje);
+
+/* Interpolacion  */
+mensaje = `el resultado de la suma de ${numeroUno} + ${numeroDos} = ${suma}`;
+console.log(mensaje);
 
 
 
