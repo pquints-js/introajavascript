b807d4621a0040fe27a1706960c8d673fa474f9b
paolo <paoloquintiliani91@gmail.com>
2024-07-22 10:37:00 -0400

    ejemplo banco

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,24 @@ cadena1="Nuevo dato";
 /* declarar e inicializar */
 var cadena2 = "Declarada e inicializada en un solo paso";
 
+/* tipo de dato */
+/* entero, decimal, logico, cadena de caracteres */
+/* integer, float, boolean, string */
+/* debilmente tipado js-python*/
+
+var entero;
+entero = 5;
+entero = "cadena de caracteres";
+
+/* en un banco */
+/* funcion que permite realizar transacciones entre cuentas */
+var montoIngresado = 10000;  /* si lo ponemos en comillas concatenan y estariamos cagados */
+var saldo = 10000;
+saldo = saldo + montoIngresado; // 20000
+
+
+
+
 
 /* funciones preescritas 
 alert();
